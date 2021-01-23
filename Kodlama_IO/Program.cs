21d4210df62597f560e4571806fe67763c0a2d6f
emreerdall <emreerdal465@gmail.com>
2021-01-23 19:17:58 +0300

    KodlamaIO

---
+++ Kodlama_IO/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Kodlama_IO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
